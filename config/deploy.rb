ce30ad47670332f753bb6da070d20aca9e6f2a86
maruchan76 <city.mati76@gmail.com>
2019-08-02 19:53:21 +0900

    デプロイ関係

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+lock "~> 3.11.0"
+set :application, 'mercari-56a'
+set :repo_url,  'git@github.com:maruchan76/mercari-56a.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+  set :linked_files, fetch(:linked_files, []).push("config/master.key")

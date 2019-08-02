db7a818a7e905a6a27b1f62c90a9317f72310665
maruchan76 <city.mati76@gmail.com>
2019-08-03 02:13:05 +0900

    BASIC認証 関連7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,18 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth, if: :production?
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   private
 
-  # def production?
-  #   Rails.env.production?
-  # end
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
 
     authenticate_or_request_with_http_basic do |username, password|
-    # http_basic_authenticate_with do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+
   end
 end

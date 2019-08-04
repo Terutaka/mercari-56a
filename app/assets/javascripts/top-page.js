805e33e86ee9ce75218e7e0ee56dc413a48ebe1f
shunsuke110 <lasvegasuver110@gmail.com>
2019-08-04 14:51:56 +0900

    top-page-main

---
+++ app/assets/javascripts/top-page.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+  $('.slides').flexslider({
+    animation: "slide",
+    autoplay:true,
+    autoplaySpeed:2000,
+    // dots:true,
+    slidesToShow:3,
+    arrows: false,
+  });
+});

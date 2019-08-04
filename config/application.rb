d643d17cc9b49b9164c1345b07f10191cb4d16ae
maruchan76 <city.mati76@gmail.com>
2019-08-04 21:10:40 +0900

    コンパイル画像関連の変更

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Mercari56a
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+  # ========== 各種画像形式をAssets ==========
+  Rails.application.config.assets.precompile += %w(*.eot *.woff *.woff2 *.ttf *.svg *.otf *.png *.jpg *.gif )
   end
 end

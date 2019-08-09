8b21039d06442a99e146442b446fc1fbcc2a0061
yoshifumi <yosshan.a.yoshi@i.softbank.jp>
2019-08-09 19:36:37 +0900

    mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,28 @@
 Rails.application.routes.draw do
-  get 'card/new'
-  get 'card/show'
-  devise_for :users
+  # get 'card/new'
+  # get 'card/show'
+  # devise_for :users
+  # root 'items#index'
+
+  # # 今後変更予定
+  # resources :items do
+  #   collection do
+  #     get "profile"
+  #     get "mypage"
+  #     get "exhibit"
+  #   end
+  # end
+
+  # resources :users
+
+  # resources :card, only: [:new, :show] do
+  #   collection do
+  #     post 'show', to: 'card#show'
+  #     post 'pay', to: 'card#pay'
+  #     post 'delete', to: 'card#delete'
+  #   end
+  # end
   root 'items#index'
+  resources :items
 
-  # 今後変更予定
-  resources :items do
-    collection do
-      get "profile"
-      get "mypage"
-      get "exhibit"
-    end
-  end
-
-  resources :users
-
-  resources :card, only: [:new, :show] do
-    collection do
-      post 'show', to: 'card#show'
-      post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
-    end
-  end
 end

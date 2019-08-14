8cf0f526e33634618e1f887a1c6058ca63ead895
shunsuke110 <lasvegasuver110@gmail.com>
2019-08-14 11:50:10 +0900

    商品一覧表示#01

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 
   def index
     # @parents = Category.all.order("id ASC").limit(13)
+    @items = Item.order("created_at DESC").limit(4)
   end
 
   def show

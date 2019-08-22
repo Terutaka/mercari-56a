2187680247dcc9283f9c1364352ce829988a5202
maruchan76 <city.mati76@gmail.com>
2019-08-22 17:50:02 +0900

    微修正

--- db/migrate/20190807075415_devise_create_users.rb
+++ db/migrate/20190807075415_devise_create_users.rb
@@ -5,14 +5,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
 
       # ユーザー新規作成時は以下のをコメントアウト解除してください
-      # t.integer :user_detail_id, foreign_key: true,null: false
+      t.integer :user_detail_id, foreign_key: true,null: false
       t.string  :nickname,               null: false, unique: true, index: true
-      # t.string  :last_name,              null: false
-      # t.string  :lfirst_name,              null: false
-      # t.string  :last_name_kana,              null: false
-      # t.string  :first_name_kana,              null: false
-      # t.integer  :birthday, null: false
-      # t.text    :profile
+      t.string  :last_name,              null: false
+      t.string  :first_name,              null: false
+      t.string  :last_name_kana,              null: false
+      t.string  :first_name_kana,              null: false
+      t.integer  :birthday, null: false
+      t.text    :profile
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""

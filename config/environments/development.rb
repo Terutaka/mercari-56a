ef02e2e0718011cceb54b5466b02dd8b18e6dedf
maruchan76 <city.mati76@gmail.com>
2019-08-23 16:22:07 +0900

    tmp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.active_storage.service = :local
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false

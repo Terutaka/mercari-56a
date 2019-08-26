697217f6d05fbcbcd7592573a6731db462f7d52d
yoshifumi <yosshan.a.yoshi@i.softbank.jp>
2019-08-26 18:30:23 +0900

    複数モデルへデータ登録/controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+    @user.build_user_details
+  end
+
+
   def edit
   end
 
@@ -15,7 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :email)
+    params.require(:user).permit(:name, user_details_attributes:[:nickname, :email, :user_id, :postal_code, :address, :building,:phone_number, :prefecture_id, :city, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :profile])
     # params.require(:user).permit(:name, :email)
   end
 end
